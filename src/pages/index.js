75314b56aa2e1306e7dad98fad1b59b49f3ab273
ZiyuTao <ziyu.tao@merico.dev>
2022-09-11 17:32:31 +0800

    feat: overhaul and section head
    new vi

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,22 @@
 import React from 'react';
-import clsx from 'clsx';
 import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-      </div>
-    </header>
-  );
-}
+import { HomepageHeader } from '@site/src/components/Sections/HomepageHeader';
+import { WhyDevlake } from '../components/Sections/WhyDevlake';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+  const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="Apache DevLake is an open-source dev data platform that ingests, analyzes, and visualizes the fragmented data from DevOps tools to distill insights for engineering productivity.">
-      <HomepageHeader />
-      <main>
-        <HomepageFeatures />
-      </main>
+      <div className='max-w-[1280px] mx-auto'>
+        <HomepageHeader />
+        <main>
+          <WhyDevlake />
+        </main>
+      </div>
     </Layout>
   );
 }
