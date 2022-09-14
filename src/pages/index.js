daf092116d41c53b9de7a5708958290bc9b3a620
Yumeng Wang <yumeng.wang@merico.dev>
2022-09-14 18:56:01 +0800

    Revert "Overhaul/new vi" (#205)
    * Revert "feat: done"
    
    This reverts commit e7ba6a0f23282109e17d5bea53c163e3f8a82998.
    
    * Revert "feat: use case mobile"
    
    This reverts commit 0d90f7efeb8606ef4160fe962c78e2695051ae47.
    
    * Revert "fix: mobile adapt header"
    
    This reverts commit 249986fc7a371422c2187729926fa5979d727b8f.
    
    * Revert "feat: mobile remove oversized asset"
    
    This reverts commit fe6f616f1c6aab1cbb46d17a7d681128dd707508.
    
    * Revert "fix: button"
    
    This reverts commit a73e776a6b15d38ee5336f3632f37e734a2a97ed.
    
    * Revert "feat: discover"
    
    This reverts commit a44ad99b6ca6dfbc324aeec6d59f9aa686dfb912.
    
    * Revert "feat: join community"
    
    This reverts commit ea0641bfa4cbb8599e47d626e97100e4798b3913.
    
    * Revert "feat: user flow"
    
    This reverts commit f58bcd582f130d70a70c725bb7615a8db3a5effe.
    
    * Revert "feat: use case"
    
    This reverts commit 4cb13c94ac6b991677cc4d691bab937f5730deef.
    
    * Revert "feat: Support section fix: header SVG -> PNG"
    
    This reverts commit 7cadc656e1347fb6e2cec0119b49892fe2d749e9.
    
    * Revert "feat: desc section"
    
    This reverts commit b211ef99191d7a7be16eb70bf5b749c507763750.
    
    * Revert "fix: minor change"
    
    This reverts commit edc76d54ca4aba655ddf22edc42de5f832d6c7ff.
    
    * Revert "fix: minor adjust"
    
    This reverts commit 33a7e548fa592d7631922f0580705828fdfb7838.
    
    * Revert "feat: overhaul and section head"
    
    This reverts commit 75314b56aa2e1306e7dad98fad1b59b49f3ab273.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,33 @@
 import React from 'react';
+import clsx from 'clsx';
 import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
-import { HomepageHeader } from '@site/src/components/Sections/HomepageHeader';
-import { WhyDevlake } from '../components/Sections/WhyDevlake';
-import { UseCases } from '../components/Sections/UseCases';
-import { UserFlow } from '../components/Sections/UserFlow';
-import { JoinCommunity } from '../components/Sections/JoinCommunity';
-import { Discover } from '../components/Sections/Discover';
+import HomepageFeatures from '../components/HomepageFeatures';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
+}
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={`${siteConfig.title}`}
       description="Apache DevLake is an open-source dev data platform that ingests, analyzes, and visualizes the fragmented data from DevOps tools to distill insights for engineering productivity.">
-      <div className='bg-[#f8f8f8]'>
-        <div className='max-w-[1280px] mx-auto bg-white'>
-          <HomepageHeader />
-          <main>
-            <WhyDevlake />
-            <UseCases />
-            <UserFlow />
-            <JoinCommunity />
-            <Discover />
-          </main>
-        </div>
-      </div>
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
     </Layout>
   );
 }
