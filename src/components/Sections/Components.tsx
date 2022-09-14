6420c7e8885279fcb9f9245ccc3407456250c68f
Yumeng Wang <yumeng.wang@merico.dev>
2022-09-14 18:39:24 +0800

    Revert "feat: Support section fix: header SVG -> PNG"
    This reverts commit 7cadc656e1347fb6e2cec0119b49892fe2d749e9.

--- src/components/Sections/Components.tsx
+++ src/components/Sections/Components.tsx
@@ -7,12 +7,6 @@ export const TextTitle = ({ children }: { children: React.ReactNode }) =>
     {children}
   </span>
 
-export const H3Title = ({ children }: { children: React.ReactNode }) =>
-  <span className="block font-inter
-  text-heading3 text-primary-800 font-semibold mb-[24px]">
-    {children}
-  </span>
-
 export const TextDescription = ({ children }: { children: React.ReactNode }) =>
   <p className="font-inter pr-[20px]
     text-label18 text-neutral-500 ">
@@ -41,27 +35,3 @@ export const SvgImg = ({ svg: Svg }: { svg: React.ComponentType<React.SVGProps<S
   >
     <Svg role="img" />
   </div>
-export const SectionImg = ({ src }: { src: string }) => <img src={src} alt="" className="
-  w-[384px] h-[288px]
-" />
-
-export const InlineLink = ({ link, children }: { link: string, children: React.ReactNode }) =>
-  <a className="text-label16 text-primary-500 mt-[16px]"
-    href={link}>{children}
-  </a>
-
-export const BreakLine = () => <div className="
-  h-[2px] bg-neutral-100 mx-[140px]
-  " />
-
-export const Sup = ({ children, title }: { children: React.ReactNode, title: string }) =>
-  <span className="w-[100px] flex flex-col items-center">
-    {children}
-    <span className="font-inter text-label14 
-      flex items-center h-[24px]
-    ">{title}</span>
-  </span>
-export const SupImg = ({ src }: { src: string }) =>
-  <img src={src} className='
-    w-[80px] h-[80px]
-  '/>
--- src/components/Sections/WhyDevlake.tsx
+++ src/components/Sections/WhyDevlake.tsx
@@ -1,51 +1,15 @@
 import React from "react";
 import * as C from "./Components";
-import WAD1 from "@site/static/img/Homepage/WAD-1.png";
-import WAD2 from "@site/static/img/Homepage/WAD-2.png";
-import WAD3 from "@site/static/img/Homepage/WAD-3.png";
-import Sup1 from "@site/static/img/Homepage/sup-1-jira.png";
-import Sup2 from "@site/static/img/Homepage/sup-2-tapd.svg";
-import Sup3 from "@site/static/img/Homepage/sup-3-github.svg";
-import Sup4 from "@site/static/img/Homepage/sup-4-gitlab.svg";
-import Sup5 from "@site/static/img/Homepage/sup-5-bitbucket.svg";
-import Sup6 from "@site/static/img/Homepage/sup-6-gitee.svg";
-import Sup7 from "@site/static/img/Homepage/sup-7-jenkins.svg";
-import Sup8 from "@site/static/img/Homepage/sup-8-ghaction.svg";
-import Sup9 from "@site/static/img/Homepage/sup-9-glci.svg";
-import SupA from "@site/static/img/Homepage/sup-A-feishu.png";
+import WAD1 from "@site/static/img/Homepage/WAD-1.svg";
+import WAD2 from "@site/static/img/Homepage/WAD-2.svg";
+import WAD3 from "@site/static/img/Homepage/WAD-3.svg";
 
 function Feature({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex w-full justify-between items-center">
+    <div className="flex w-full justify-between items-center space-y-7">
       {children}
     </div>
   );
-}
-
-function Supports() {
-  return (
-    <div className=" flex flex-col items-center
-      mt-[48px]
-    ">
-      <C.H3Title>Supported Data Sources</C.H3Title>
-      <div className="grid grid-cols-5 gap-x-[24px] gap-y-[16px]">
-        <C.Sup title="Jira"><C.SupImg src={Sup1} /></C.Sup>
-        <C.Sup title="TAPD"><Sup2 /></C.Sup>
-        <C.Sup title="GitHub"><Sup3 /></C.Sup>
-        <C.Sup title="GitLab"><Sup4 /></C.Sup>
-        <C.Sup title="BitBucket"><Sup5 /></C.Sup>
-        <C.Sup title="Gitee"><Sup6 /></C.Sup>
-        <C.Sup title="Jenkins"><Sup7 /></C.Sup>
-        <C.Sup title="GitHub Action"><Sup8 /></C.Sup>
-        <C.Sup title="GitLab CI"><Sup9 /></C.Sup>
-        <C.Sup title="Feishu"><C.SupImg src={SupA} /></C.Sup>
-      </div>
-      <div className="text-label18 font-inter text-neutral-500
-        mt-[48px]">
-        <C.InlineLink link="https://devlake.apache.org/docs/Overview/Roadmap">More data sources</C.InlineLink> comming soon...
-      </div>
-    </div>
-  )
 }
 
 export function WhyDevlake() {
@@ -58,11 +22,11 @@ export function WhyDevlake() {
     <div
       className="
         flex flex-col
-        px-[140px] mb-[72px]
-        space-y-7
+        px-[140px]
+        space-y-5
         ">
       <Feature>
-        <C.SectionImg src={WAD1} />
+        <C.SvgImg svg={WAD1} />
         <C.TextSection>
           <C.TextTitle>Data Silos Connected</C.TextTitle>
           <C.TextDescription>
@@ -81,10 +45,10 @@ export function WhyDevlake() {
             <C.TextLink link="https://devlake.apache.org/docs/LiveDemo">Interact with pre-built dashboards</C.TextLink>
           </C.TextDescription>
         </C.TextSection>
-        <C.SectionImg src={WAD2} />
+        <C.SvgImg svg={WAD2} />
       </Feature>
       <Feature>
-        <C.SectionImg src={WAD3} />
+        <C.SvgImg svg={WAD3} />
         <C.TextSection>
           <C.TextTitle>A Highly Flexible Framework</C.TextTitle>
           <C.TextDescription>
@@ -95,7 +59,6 @@ export function WhyDevlake() {
         </C.TextSection>
       </Feature>
     </div>
-    <C.BreakLine />
-    <Supports />
   </section>)
 }
+
