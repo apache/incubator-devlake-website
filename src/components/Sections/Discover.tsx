3bd2ecafb53404cbfa902c8d47484eb8ef4e4dd6
Yumeng Wang <yumeng.wang@merico.dev>
2022-09-14 18:39:24 +0800

    Revert "fix: button"
    This reverts commit a73e776a6b15d38ee5336f3632f37e734a2a97ed.

--- src/components/Sections/Discover.tsx
+++ src/components/Sections/Discover.tsx
@@ -5,13 +5,13 @@ import BG from "@site/static/img/Homepage/discover.png";
 
 export function Discover() {
   return (<div className="flex flex-col items-center relative h-[324px] font-inter bg-primary-100">
-    <img src={BG} alt='' className="absolute bottom-[0px] z-0" />
+    <img src={BG} alt='' className="absolute bottom-[0px]" />
     <div className="font-medium text-primary-800 
       text-[32px] leading-[48px] mt-[72px]">Discover more engineering insights with</div>
     <div className="font-semibold text-primary-500
       text-[40px] leading-[60px] mt-1 mb-3">Apache DevLake (Incubating)</div>
     <Link
-      className="primary-button z-10"
+      className="primary-button"
       to="https://devlake.apache.org/docs/GettingStarted"
     >
       Getting Started
